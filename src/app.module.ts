35e05277814ae08c7bc411ddda79803c0a69b50c
Edouard Bougon <edouard@bougon.fr>
2022-03-05 18:05:05 +0100

    feat(config): add config module for injection

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,14 +4,14 @@ import { APP_INTERCEPTOR } from '@nestjs/core';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AlgoanModule } from './algoan/algoan.module';
+import { ConfigModule } from './config/config.module';
 import { HooksModule } from './hooks/hooks.module';
 
 /**
  * App module
  */
 @Module({
-  imports: [AlgoanModule, HooksModule],
+  imports: [ConfigModule, HooksModule],
   controllers: [AppController],
   providers: [
     AppService,
