aa162ba8571fb5a9e0f08c95c83dcab58bfb12ef
Edouard Bougon <edouard@bougon.fr>
2022-03-27 00:34:23 +0100

    feat(hook): update services and dto

---
+++ src/hooks/mappers/analysis.mapper.ts
@@ -0,0 +1,62 @@
+/* eslint-disable prefer-arrow/prefer-arrow-functions */
+import { OxlinAccount } from 'src/oxlin/dto/account.object';
+import { OxlinConnection } from 'src/oxlin/dto/connection.object';
+import { OxlinTransaction } from 'src/oxlin/dto/transaction.object';
+import { AnalysisFormat, AnalysisStatus, ErrorCodes } from '../../algoan/dto/analysis.enum';
+import { OxlinAccountApiV2AnalysisUpdateInput } from '../../algoan/dto/analysis.inputs';
+
+/**
+ * Default currency if no provided
+ */
+export const defaultCurrency: string = 'EUR';
+
+/**
+ * Map Oxlin Data To Algoan Analysis
+ */
+export function mapOxlinDataToAlgoanAnalysis(
+  connection: OxlinConnection,
+  accounts: OxlinAccount[],
+  transactions: OxlinTransaction[],
+): OxlinAccountApiV2AnalysisUpdateInput<OxlinConnection, OxlinAccount, OxlinTransaction> {
+  // Group transactions by accountId
+  const transactionsByAccountId: Map<string, OxlinTransaction[]> = transactions.reduce(
+    (map: Map<string, OxlinTransaction[]>, t: OxlinTransaction) => {
+      map.set(t.account_id, [...(map.get(t.account_id) ?? []), t]);
+
+      return map;
+    },
+    new Map(),
+  );
+
+  return {
+    status: AnalysisStatus.COMPLETED,
+    format: AnalysisFormat.OXLIN_ACCOUNT_API_V2,
+    connections: [
+      {
+        ...connection,
+        accounts: accounts.map((account: OxlinAccount) => ({
+          ...account,
+          transactions: transactionsByAccountId.get(account.id) ?? [],
+        })),
+      },
+    ],
+  };
+}
+
+/**
+ * Map Oxlin Error To Algoan Analysis
+ */
+export function mapOxlinErrorToAlgoanAnalysis(
+  message: string,
+  connection?: OxlinConnection,
+): OxlinAccountApiV2AnalysisUpdateInput<OxlinConnection, OxlinAccount, OxlinTransaction> {
+  return {
+    format: AnalysisFormat.OXLIN_ACCOUNT_API_V2,
+    status: AnalysisStatus.ERROR,
+    error: {
+      code: ErrorCodes.INTERNAL_ERROR,
+      message,
+    },
+    connections: connection !== undefined ? [{ ...connection, accounts: [] }] : [],
+  };
+}
--- src/hooks/services/hooks.service.spec.ts
+++ src/hooks/services/hooks.service.spec.ts
@@ -1,3 +1,4 @@
+/* eslint-disable max-lines */
 /* eslint-disable @typescript-eslint/naming-convention,camelcase */
 import {
   Algoan,
@@ -13,6 +14,16 @@ import { config } from 'node-config-ts';
 import { ContextIdFactory } from '@nestjs/core';
 import { createMock } from '@golevelup/ts-jest';
 
+import { OxlinConnectionStatus } from '../../oxlin/dto/connection.enums';
+import { oxlinUserMock } from '../../oxlin/dto/user.object.mock';
+import { oxlinConnectionMock } from '../../oxlin/dto/connection.object.mock';
+import { oxlinAccountsMock } from '../../oxlin/dto/account.object.mock';
+import { oxlinTransactionsMock } from '../../oxlin/dto/transaction.object.mock';
+import { AnalysisFormat, AnalysisStatus, ErrorCodes } from '../../algoan/dto/analysis.enum';
+import { analysisMock } from '../../algoan/dto/analysis.objects.mock';
+import { OxlinAccountService } from '../../oxlin/services/oxlin-account.service';
+import { OxlinConnectionService } from '../../oxlin/services/oxlin-connection.service';
+import { AlgoanAnalysisService } from '../../algoan/services/algoan-analysis.service';
 import { OxlinAuthService } from '../../oxlin/services/oxlin-auth.service';
 import { OxlinModule } from '../../oxlin/oxlin.module';
 import { OxlinLinkService } from '../../oxlin/services/oxlin-link.service';
@@ -29,6 +40,8 @@ import { AggregationDetailsAggregatorName, AggregationDetailsMode } from '../../
 import { aggregatorLinkRequiredMock } from '../dto/aggregator-link-required-payload.dto.mock';
 import { subscriptionMock } from '../dto/subscription.dto.mock';
 
+import { bankDetailsRequiredMock } from '../dto/bank-details-required-payload.dto.mock';
+import { mapOxlinDataToAlgoanAnalysis } from '../mappers/analysis.mapper';
 import { HooksService } from './hooks.service';
 
 describe('HookService', () => {
@@ -36,9 +49,12 @@ describe('HookService', () => {
   let algoanService: AlgoanService;
   let algoanHttpService: AlgoanHttpService;
   let algoanCustomerService: AlgoanCustomerService;
+  let algoanAnalysisService: AlgoanAnalysisService;
   let oxlinLinkService: OxlinLinkService;
   let oxlinUserService: OxlinUserService;
   let oxlinAuthService: OxlinAuthService;
+  let oxlinConnectionService: OxlinConnectionService;
+  let oxlinAccountService: OxlinAccountService;
   let serviceAccount: ServiceAccount;
 
   beforeEach(async () => {
@@ -75,9 +91,12 @@ describe('HookService', () => {
     algoanService = await moduleRef.resolve<AlgoanService>(AlgoanService, contextId);
     algoanHttpService = await moduleRef.resolve<AlgoanHttpService>(AlgoanHttpService, contextId);
     algoanCustomerService = await moduleRef.resolve<AlgoanCustomerService>(AlgoanCustomerService, contextId);
+    algoanAnalysisService = await moduleRef.resolve<AlgoanAnalysisService>(AlgoanAnalysisService, contextId);
     oxlinAuthService = await moduleRef.resolve<OxlinAuthService>(OxlinAuthService, contextId);
     oxlinUserService = await moduleRef.resolve<OxlinUserService>(OxlinUserService, contextId);
     oxlinLinkService = await moduleRef.resolve<OxlinLinkService>(OxlinLinkService, contextId);
+    oxlinConnectionService = await moduleRef.resolve<OxlinConnectionService>(OxlinConnectionService, contextId);
+    oxlinAccountService = await moduleRef.resolve<OxlinAccountService>(OxlinAccountService, contextId);
     serviceAccount = await moduleRef.resolve<ServiceAccount>(ServiceAccount, contextId);
 
     jest.spyOn(Algoan.prototype, 'initRestHooks').mockResolvedValue();
@@ -114,9 +133,7 @@ describe('HookService', () => {
       getCustomerByIdSpy = jest.spyOn(algoanCustomerService, 'getCustomerById').mockResolvedValue(customerMock);
       geClientTokenSpy = jest.spyOn(oxlinAuthService, 'geClientToken').mockResolvedValue(`client-token-${process.pid}`);
       getUserTokenSpy = jest.spyOn(oxlinAuthService, 'getUserToken').mockResolvedValue(`user-token-${process.pid}`);
-      getUserSpy = jest
-        .spyOn(oxlinUserService, 'getUser')
-        .mockResolvedValue({ id: `id-${process.pid}`, email: 'email' });
+      getUserSpy = jest.spyOn(oxlinUserService, 'getUser').mockResolvedValue(oxlinUserMock);
       createNewUserSpy = jest.spyOn(oxlinUserService, 'createNewUser').mockResolvedValue(`id-${process.pid}`);
       getIframeUrlSpy = jest.spyOn(oxlinLinkService, 'getIframeUrl').mockResolvedValue('MY_LINK_URL');
     });
@@ -309,6 +326,219 @@ describe('HookService', () => {
           aggregatorName: AggregationDetailsAggregatorName.oxlin,
         },
       });
+    });
+  });
+
+  describe('handleBankDetailsRequiredEvent', () => {
+    let algoanAuthenticateSpy: jest.SpyInstance;
+    let getUserTokenSpy: jest.SpyInstance;
+    let deleteUserTokenSpy: jest.SpyInstance;
+    let getCustomerByIdSpy: jest.SpyInstance;
+    let getConnectionByIdSpy: jest.SpyInstance;
+    let getAllAccountsForConnectionSpy: jest.SpyInstance;
+    let getAllTransactionsForAllAccountsSpy: jest.SpyInstance;
+    let updateAnalysisSpy: jest.SpyInstance;
+    let getUserSpy: jest.SpyInstance;
+
+    beforeEach(async () => {
+      algoanAuthenticateSpy = jest.spyOn(algoanHttpService, 'authenticate').mockReturnValue();
+      getCustomerByIdSpy = jest.spyOn(algoanCustomerService, 'getCustomerById').mockResolvedValue(customerMock);
+      updateAnalysisSpy = jest.spyOn(algoanAnalysisService, 'updateAnalysis').mockResolvedValue(analysisMock);
+      getUserTokenSpy = jest.spyOn(oxlinAuthService, 'getUserToken').mockResolvedValue(`user-token-${process.pid}`);
+      getUserSpy = jest.spyOn(oxlinUserService, 'getUser').mockResolvedValue(oxlinUserMock);
+      deleteUserTokenSpy = jest.spyOn(oxlinUserService, 'deleteUser').mockResolvedValue();
+      getConnectionByIdSpy = jest
+        .spyOn(oxlinConnectionService, 'getConnectionWithFinalStatus')
+        .mockResolvedValue(oxlinConnectionMock);
+      getAllAccountsForConnectionSpy = jest
+        .spyOn(oxlinAccountService, 'getAllAccountsForConnection')
+        .mockResolvedValue(oxlinAccountsMock);
+      getAllTransactionsForAllAccountsSpy = jest
+        .spyOn(oxlinAccountService, 'getAllTransactionsForAllAccounts')
+        .mockResolvedValue(oxlinTransactionsMock);
+    });
+
+    it('should throw error if client config missing', async () => {
+      serviceAccount.config = undefined;
+      await expect(
+        hookService.handleBankDetailsRequiredEvent(bankDetailsRequiredMock, new Date()),
+      ).rejects.toThrowError(`Missing information: clientConfig: undefined`);
+
+      expect(updateAnalysisSpy).toHaveBeenCalledWith(
+        bankDetailsRequiredMock.customerId,
+        bankDetailsRequiredMock.analysisId,
+        {
+          format: AnalysisFormat.OXLIN_ACCOUNT_API_V2,
+          status: AnalysisStatus.ERROR,
+          error: {
+            code: ErrorCodes.INTERNAL_ERROR,
+            message: `An error occured when fetching data from the aggregator`,
+          },
+          connections: [],
+        },
+      );
+    });
+
+    it('should do these steps WITHOUT an existing oxlin user', async () => {
+      await expect(
+        hookService.handleBankDetailsRequiredEvent(bankDetailsRequiredMock, new Date()),
+      ).rejects.toThrowError("Oxlin user id is not defined, can't connect to Oxlin");
+
+      expect(updateAnalysisSpy).toHaveBeenCalledWith(
+        bankDetailsRequiredMock.customerId,
+        bankDetailsRequiredMock.analysisId,
+        {
+          format: AnalysisFormat.OXLIN_ACCOUNT_API_V2,
+          status: AnalysisStatus.ERROR,
+          error: {
+            code: ErrorCodes.INTERNAL_ERROR,
+            message: `An error occured when fetching data from the aggregator`,
+          },
+          connections: [],
+        },
+      );
+    });
+
+    it('should do these steps WITH an existing oxlin user and WITH WRONG connection status', async () => {
+      // mock to return an existing userId
+      getCustomerByIdSpy = jest.spyOn(algoanCustomerService, 'getCustomerById').mockResolvedValue({
+        ...customerMock,
+        aggregationDetails: {
+          ...customerMock.aggregationDetails,
+          userId: `userId-${process.pid}`,
+        },
+      });
+
+      getConnectionByIdSpy.mockResolvedValue({
+        ...oxlinConnectionMock,
+        status: OxlinConnectionStatus.FAILED,
+      });
+
+      await hookService.handleBankDetailsRequiredEvent(bankDetailsRequiredMock, new Date());
+
+      // Start to get Oxlin data
+      // first get connection
+      expect(getConnectionByIdSpy).toHaveBeenCalledWith(
+        `user-token-${process.pid}`,
+        oxlinUserMock.id,
+        bankDetailsRequiredMock.temporaryCode,
+        serviceAccountConfigMock.finalConnectionTimeoutInMS,
+      );
+
+      expect(updateAnalysisSpy).toHaveBeenCalledWith(
+        bankDetailsRequiredMock.customerId,
+        bankDetailsRequiredMock.analysisId,
+        {
+          format: AnalysisFormat.OXLIN_ACCOUNT_API_V2,
+          status: AnalysisStatus.ERROR,
+          error: {
+            code: ErrorCodes.INTERNAL_ERROR,
+            message: `Fail to get connection with a valid status. Received ${OxlinConnectionStatus.FAILED}`,
+          },
+          connections: [{ ...oxlinConnectionMock, status: OxlinConnectionStatus.FAILED, accounts: [] }],
+        },
+      );
+
+      // but not accounts
+      expect(getAllAccountsForConnectionSpy).not.toHaveBeenCalled();
+
+      // neither transactions
+      expect(getAllTransactionsForAllAccountsSpy).not.toHaveBeenCalled();
+
+      // And finally we DIDN'T delete the user
+      expect(deleteUserTokenSpy).not.toHaveBeenCalled();
+    });
+
+    it('should do these steps if Oxlin return an error', async () => {
+      // mock to return an existing userId
+      getCustomerByIdSpy = jest.spyOn(algoanCustomerService, 'getCustomerById').mockResolvedValue({
+        ...customerMock,
+        aggregationDetails: {
+          ...customerMock.aggregationDetails,
+          userId: `userId-${process.pid}`,
+        },
+      });
+
+      getAllTransactionsForAllAccountsSpy.mockRejectedValue(new Error('bla bla'));
+
+      await expect(
+        hookService.handleBankDetailsRequiredEvent(bankDetailsRequiredMock, new Date()),
+      ).rejects.toThrowError('bla bla');
+
+      expect(updateAnalysisSpy).toHaveBeenCalledWith(
+        bankDetailsRequiredMock.customerId,
+        bankDetailsRequiredMock.analysisId,
+        {
+          format: AnalysisFormat.OXLIN_ACCOUNT_API_V2,
+          status: AnalysisStatus.ERROR,
+          error: {
+            code: ErrorCodes.INTERNAL_ERROR,
+            message: `An error occured when fetching data from the aggregator`,
+          },
+          connections: [],
+        },
+      );
+
+      // And finally we DIDN'T delete the user
+      expect(deleteUserTokenSpy).not.toHaveBeenCalled();
+    });
+
+    it('should do these steps WITH an existing oxlin user and WITHOUT Oxlin error', async () => {
+      // mock to return an existing userId
+      getCustomerByIdSpy = jest.spyOn(algoanCustomerService, 'getCustomerById').mockResolvedValue({
+        ...customerMock,
+        aggregationDetails: {
+          ...customerMock.aggregationDetails,
+          userId: `userId-${process.pid}`,
+        },
+      });
+
+      await hookService.handleBankDetailsRequiredEvent(bankDetailsRequiredMock, new Date());
+
+      // Should try to get the existing user
+      expect(getUserTokenSpy).toHaveBeenCalledWith(
+        serviceAccountConfigMock.clientId,
+        serviceAccountConfigMock.clientSecret,
+        algoanCustomerService.getDefaultEmail(customerMock.id),
+        algoanCustomerService.getDefaultPassword(customerMock.id),
+      );
+      expect(getUserSpy).toHaveBeenCalledWith(`user-token-${process.pid}`, `userId-${process.pid}`);
+
+      // then get a user token
+      expect(getUserTokenSpy).toHaveBeenCalledWith(
+        serviceAccountConfigMock.clientId,
+        serviceAccountConfigMock.clientSecret,
+        algoanCustomerService.getDefaultEmail(customerMock.id),
+        algoanCustomerService.getDefaultPassword(customerMock.id),
+      );
+
+      // Start to get Oxlin data
+      // first get connection
+      expect(getConnectionByIdSpy).toHaveBeenCalledWith(
+        `user-token-${process.pid}`,
+        oxlinUserMock.id,
+        bankDetailsRequiredMock.temporaryCode,
+        serviceAccountConfigMock.finalConnectionTimeoutInMS,
+      );
+
+      // then accounts
+      expect(getAllAccountsForConnectionSpy).toHaveBeenCalledWith(`user-token-${process.pid}`, oxlinConnectionMock.id);
+
+      // and then transactions for all accounts
+      expect(getAllTransactionsForAllAccountsSpy).toHaveBeenCalledWith(`user-token-${process.pid}`, [
+        oxlinAccountsMock[0].id,
+        oxlinAccountsMock[1].id,
+      ]);
+
+      // We can now update the analysis with oxlin data
+      expect(updateAnalysisSpy).toHaveBeenCalledWith(
+        bankDetailsRequiredMock.customerId,
+        bankDetailsRequiredMock.analysisId,
+        mapOxlinDataToAlgoanAnalysis(oxlinConnectionMock, oxlinAccountsMock, oxlinTransactionsMock),
+      );
+
+      // And finally we can delete the user
+      expect(deleteUserTokenSpy).toHaveBeenCalledWith(`user-token-${process.pid}`, oxlinUserMock.id);
     });
   });
 });
